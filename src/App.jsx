e090b551b8c1fd668681f6533b0c761cfc0802db
Work Goonja <workgoonja@goonjas-MacBook-Pro.local>
2025-07-11 21:54:46 +0530

    Styling of logo changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,22 @@
 import "./App.scss";
-import logo from "./assets/crown.svg";
-import CardList from "./components/cards/card-list/card-list.component";
+import { Routes, Route } from "react-router-dom";
+import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+
+function Cart() {
+  return (
+    <h1>This is cart</h1>
+  )
+}
 
 function App() {
-  const categoriesApp = [
-    {
-      id: 1,
-      title: "Hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "Jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "Sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "Womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "Mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
   return (
-    <>
-      <span className="header-logo">
-        <img src={logo} />
-      </span>
-      <div className="container">
-        <CardList categories={categoriesApp} />
-      </div>
-    </>
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />}/>
+        <Route path="cart" element={<Cart />}/>
+      </Route>
+    </Routes>
   );
 }
 
