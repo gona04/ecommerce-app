00960c692753f03cf3dd46ed58fa53844ac7b34a
Work Goonja <gandhi.goonja@gmail.com>
2025-07-12 08:38:23 +0530

    This has to kind of be an empty project to practice from start

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,7 @@
-import "./App.scss";
-import { Routes, Route } from 'react-router-dom';
-import Navigation from "./routes/navigation/navigation.component";
-import Home from "./routes/home/home.component";
-
-function Cart() {
-  return (
-    <h1>Card is here</h1>
-  )
-}
-
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path="cart" element={<Cart/>}/>
-      </Route>
-    </Routes>
-  );
+return (
+  <h1></h1>
+)
 }
 
 export default App;
