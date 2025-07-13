ff57e1b1d8e79a6c3bf6c04637bcdc288057518b
Work Goonja <gandhi.goonja@gmail.com>
2025-07-14 00:08:11 +0530

    Removed the cart that was there for testing

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,6 @@ import Navigation from "./routes/navigation/navigation.component";
 import Home from "./routes/home/home.component";
 import SignIn from "./routes/sign-in/sign-in.component";
 
-function Cart() {
-  return (
-    <h1>This is cart</h1>
-  )
-}
-
 function App() {
   return (
     <Routes>
