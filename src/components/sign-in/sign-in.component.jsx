41069d6c59d24c3ee650beee3ece42f74a5bcf05
Work Goonja <gandhi.goonja@gmail.com>
2025-07-18 14:09:01 +0530

    adding try catch for google login

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,11 @@ import { useNavigate } from "react-router-dom";
 import Button from "../button/button.component";
 import FormInput from "../form-input/form-input.component";
 import "./sign-in.styles.scss";
-import { createUser, emailPasswordSignin, signInGoogle } from "../../utils/firebase/firebase.utils";
+import {
+  createUser,
+  emailPasswordSignin,
+  signInGoogle,
+} from "../../utils/firebase/firebase.utils";
 import { UserContext } from "../../context/user.context";
 
 const defaultFormFields = {
@@ -11,33 +15,39 @@ const defaultFormFields = {
   password: "",
 };
 
-const errorMessage = new Map()
-.set('auth/invalid-credential', 'Please enter correct email or password')
+const errorMessage = new Map().set(
+  "auth/invalid-credential",
+  "Please enter correct email or password"
+);
 
 function SignIn() {
   const [formFields, setFormFields] = useState(defaultFormFields);
-  const {currentUser} = useContext(UserContext);
+  const { currentUser } = useContext(UserContext);
   const { email, password } = formFields;
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   useEffect(() => {
-    if(currentUser) navigate('/shop')
-  }, [currentUser])
+    if (currentUser) navigate("/shop");
+  }, [currentUser]);
 
   const logGoogleUser = async (e) => {
     e.preventDefault();
-    const { user } = await signInGoogle();
-    await createUser(user);
+    try {
+      const { user } = await signInGoogle();
+      await createUser(user);
+    } catch (e) {
+      console.error(e.code);
+    }
   };
 
   const signInWithEmail = async (e) => {
     e.preventDefault();
-   try {
-    const result = await emailPasswordSignin(email, password)
-    console.log(result);
-   } catch(e) {
-    console.log(e);
-    alert(errorMessage.get(e.code))
-   }
+    try {
+      const result = await emailPasswordSignin(email, password);
+      console.log(result);
+    } catch (e) {
+      console.log(e);
+      alert(errorMessage.get(e.code));
+    }
   };
 
   function handleOnChange(e) {
